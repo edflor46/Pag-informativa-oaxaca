54d7035cf22043696551f63edf1a788bf89f5dbf
Yahiko <edflor46@gmail.com>
2021-01-28 18:06:22 -0600

    Página web estatica

---
+++ README.md
@@ -0,0 +1,32 @@
+# Oxaca
+
+Página web informativa sobre el estado de Oaxaca, esta página es estática ya que su contenido está centrado en el mismo HTML
+
+![alt text][img-1]
+[img-1]: img/README/oaxaca-1.png
+
+## Tradiciones y costumbres
+![alt text][img-2]
+[img-2]: img/README/oaxaca-2.png
+
+## Lugares para visitar
+![alt text][img-3]
+[img-3]: img/README/oaxaca-3.png
+
+## Galería
+Con la librería LightBox de JavaScript se puede construir una galería de manera muy rápida
+![alt text][img-4]
+[img-4]: img/README/oaxaca-4.png
+
+![alt text][img-5]
+[img-5]: img/README/oaxaca-5.png
+
+## Responsivo
+La página es adaptable a cualquier a diferentes tamaños de pantalla 
+![alt text][img-6]
+[img-6]: img/README/oaxaca-6.png
+
+![alt text][img-7]
+[img-7]: img/README/oaxaca-7.png
+
+
---
+++ js/main.js
@@ -0,0 +1,80 @@
+
+/*Menu animado*/
+window.onscroll = function () {
+    /*console.log('eje y: ' + window.scrollY);*/
+    menuAnimado();
+}
+
+if(window.location.href.indexOf('index') > -1) {
+    function menuAnimado() {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            document.querySelector('.encabezado').style.top = '0';
+        } else {
+            document.querySelector('.encabezado').style.top = '-65px';
+        }
+    }
+} else {
+    function menuAnimado() {
+        if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+            document.querySelector('.encabezado').style.top = '0';
+        } else {
+            document.querySelector('.encabezado').style.top = '-65px';
+        }
+    }
+}
+
+
+
+
+
+/*Menu-Movil*/
+
+document.querySelector('.menu-movil').addEventListener('click', function () {
+    document.querySelector('.menu').classList.toggle('show');
+});
+
+/*Cerrar*/
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.menu').classList.toggle('show');
+});
+
+
+/*Acordeon*/
+var mostrar = document.getElementsByClassName("btn");
+var img = document.getElementsByClassName("img");
+var i;
+for (i = 0; i < mostrar.length; i++) {
+    mostrar[i].addEventListener("click", function () {
+        console.log("adasd");
+        this.classList.toggle("activo");
+
+        var mostrado = this.nextElementSibling;
+
+        if (mostrado.style.maxHeight) {
+            mostrado.style.maxHeight = null;
+
+
+
+        } else {
+            mostrado.style.maxHeight = mostrado.scrollHeight + "px";
+
+        }
+
+    });
+}
+
+
+
+
+$(document).ready(function () {
+    if (window.location.href.indexOf('index') > -1) {
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            auto: true,
+            stopAutoOnClick: true,
+            infiniteLoop: true,
+            slideWidth: 1350
+        });
+    }
+});
